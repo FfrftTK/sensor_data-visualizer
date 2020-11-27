e13832aae09b1a5f578637c9dd09ede6f7ce70cb
FfrftTK <t1610415@uec.ac.jp>
2020-11-27 22:57:43 +0900

    Add proto pages

--- lib/main.dart
+++ lib/main.dart
@@ -3,89 +3,51 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
 import 'package:hooks_riverpod/all.dart';
-import 'package:state_notifier/state_notifier.dart';
+import 'package:velocity_visualizer/providers.dart';
 
 void main() {
   runApp(ProviderScope(child: CounterApp()));
 }
-
-class Counter extends StateNotifier<int> {
-  Counter() : super(0);
-
-  void increment() => state++;
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-}
-
-final counterProvider = StateNotifierProvider.autoDispose((_) => Counter());
 
 class CounterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       getPages: getPages,
-      home: const SecondPage(),
+      home: const AccelerometerPage(),
     );
   }
 }
 
-class CounterPage extends HookWidget {
-  const CounterPage();
+class AccelerometerPage extends HookWidget {
+  const AccelerometerPage();
 
-  static const routeName = '/counter';
+  static const routeName = '/accelerometer';
 
   @override
   Widget build(BuildContext context) {
-    final state = useProvider(counterProvider.state);
-    final counter = useProvider(counterProvider);
+    final state = useProvider(accelerometerProvider.state);
+    final controller = useProvider(accelerometerProvider)..initState();
 
     return Scaffold(
-      appBar: AppBar(title: const Text('CounterApp')),
+      appBar: AppBar(title: const Text('Accelerometer')),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
             child: Text(state.toString()),
           ),
-          RaisedButton(
-            onPressed: () => Get.toNamed(SecondPage.routeName),
-            child: const Text('to other page'),
-          ),
+//          RaisedButton(
+//            onPressed: () => Get.toNamed(SecondPage.routeName),
+//            child: const Text('to other page'),
+//          ),
         ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: counter.increment,
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  const SecondPage();
-
-  static const routeName = '/second';
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            const Text('WTF'),
-            RaisedButton(
-              onPressed: () => Get.toNamed(CounterPage.routeName),
-              child: const Text('to other page'),
-            ),
-          ],
-        ),
       ),
     );
   }
 }
 
 final List<GetPage> getPages = [
-  GetPage(name: CounterPage.routeName, page: () => const CounterPage()),
-  GetPage(name: SecondPage.routeName, page: () => const SecondPage()),
+  GetPage(
+      name: AccelerometerPage.routeName, page: () => const AccelerometerPage()),
 ];
