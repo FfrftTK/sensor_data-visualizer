946dabce6abf1ce36cdaa0d568c3a9071275672c
FfrftTK <t1610415@uec.ac.jp>
2020-11-30 00:02:14 +0900

    Add operator

--- lib/states/accelerometer/accelerometer_state.dart
+++ lib/states/accelerometer/accelerometer_state.dart
@@ -23,4 +23,16 @@ class AccelerationData {
 
   final AccelerometerEvent event;
   final DateTime timestamp;
+
+  @override
+  AccelerationData operator -(AccelerometerEvent event) {
+    return AccelerationData._(
+      event: AccelerometerEvent(
+        this.event.x - event.x,
+        this.event.y - event.y,
+        this.event.z - event.z,
+      ),
+      timestamp: timestamp,
+    );
+  }
 }
