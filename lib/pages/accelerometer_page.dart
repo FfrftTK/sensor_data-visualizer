7312d05856086345f9b74ce9cc137952b93c56c4
FfrftTK <t1610415@uec.ac.jp>
2020-11-30 01:27:55 +0900

    Fix provider handling

--- lib/pages/accelerometer_page.dart
+++ lib/pages/accelerometer_page.dart
@@ -7,14 +7,16 @@ class AccelerometerPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    useProvider(accelerometerProvider.state);
     final controller = useProvider(accelerometerProvider);
 
     return Scaffold(
       appBar: AppBar(title: const Text('Accelerometer')),
       body: Container(
         padding: EdgeInsets.all(Get.width * 0.025),
+        alignment: Alignment.center,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Container(
@@ -29,44 +31,54 @@ class AccelerometerPage extends HookWidget {
                 swapAnimationDuration: const Duration(milliseconds: 0),
               ),
             ),
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(Get.width * 0.02),
-                  border: Border.all(
-                    color: Get.theme.highlightColor,
-                  ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Get.width * 0.02),
+                border: Border.all(
+                  color: Get.theme.highlightColor,
                 ),
-                constraints: BoxConstraints(
-                    maxWidth: Get.width * 0.5, maxHeight: Get.height * 0.2),
-                padding: EdgeInsets.all(Get.width * 0.02),
-                child: SingleChildScrollView(
-                  child: Column(
-                    children: controller.lineBarDetails
-                        .map(
-                          (e) => Indicator(
-                            description: e.description,
-                            colors: e.colors,
-                            size: Get.width * 0.05,
-                            textStyle:
-                                const TextStyle(fontWeight: FontWeight.bold),
-                            padding: EdgeInsets.symmetric(
-                                vertical: Get.height * 0.005),
-                          ),
-                        )
-                        .toList(),
-                  ),
+              ),
+              constraints: BoxConstraints(
+                  maxWidth: Get.width * 0.5, maxHeight: Get.height * 0.2),
+              padding: EdgeInsets.all(Get.width * 0.02),
+              margin: EdgeInsets.all(Get.width * 0.05),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: controller.lineBarDetails
+                      .map(
+                        (e) => Indicator(
+                          description: e.description,
+                          colors: e.colors,
+                          size: Get.width * 0.05,
+                          textStyle:
+                              const TextStyle(fontWeight: FontWeight.bold),
+                          padding: EdgeInsets.symmetric(
+                              vertical: Get.height * 0.005),
+                        ),
+                      )
+                      .toList(),
                 ),
               ),
             ),
-            RaisedButton(
-              onPressed: controller.updateOffset,
-              child: const Text('Set current value as offset'),
+            SizedBox(
+              width: Get.width * 0.75,
+              child: RaisedButton(
+                onPressed: controller.updateOffset,
+                child: const Text('Set current value as offset'),
+              ),
             ),
-            RaisedButton(
-              onPressed: controller.clearOffset,
-              child: const Text('Clear offset'),
+            SizedBox(
+              width: Get.width * 0.75,
+              child: RaisedButton(
+                onPressed: controller.clearOffset,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    Icon(Icons.refresh),
+                    const Text('  Clear offset'),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
