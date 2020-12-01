d4070f35e824badde0011df1fbe45fa4ada0b3a2
FfrftTK <t1610415@uec.ac.jp>
2020-12-01 18:09:36 +0900

    Fix lint

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -1,6 +1,5 @@
 library pages;
 
-import 'package:expandable/expandable.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
