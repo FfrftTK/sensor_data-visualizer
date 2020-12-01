8d75d534bb8cc5bac4086c07c76c87e8936b13ad
FfrftTK <t1610415@uec.ac.jp>
2020-12-01 18:09:08 +0900

    Add publish controllable button

--- lib/pages/accelerometer/accelerometer_page.dart
+++ lib/pages/accelerometer/accelerometer_page.dart
@@ -11,7 +11,24 @@ class AccelerometerPage extends HookWidget {
     final controller = useProvider(accelerometerProvider);
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Accelerometer')),
+      appBar: AppBar(
+        title: const Text('Accelerometer'),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.cloud_upload,
+              color: controller.cloudColor,
+            ),
+            onPressed: () async {
+              await controller.onCloudTapped();
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: () => Get.toNamed(AccelerometerConfigPage.routeName),
+          ),
+        ],
+      ),
       body: Container(
         padding: EdgeInsets.all(Get.width * 0.025),
         alignment: Alignment.center,
@@ -64,7 +81,7 @@ class AccelerometerPage extends HookWidget {
               width: Get.width * 0.75,
               child: RaisedButton(
                 onPressed: controller.updateOffset,
-                child: const Text('Set current value as offset'),
+                child: const Text('Set current observation as offset'),
               ),
             ),
             SizedBox(
--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -1,11 +1,12 @@
 library pages;
 
+import 'package:expandable/expandable.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
 import 'package:hooks_riverpod/all.dart';
 import 'package:sensor_data_visualizer/providers.dart';
-import 'package:sensor_data_visualizer/utils/utils.dart';
 
-part 'accelerometer_page.dart';
+part 'accelerometer/accelerometer_page.dart';
+part 'accelerometer/config/acccelerometer_config_page.dart';
